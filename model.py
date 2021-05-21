8be2ed24ecc7612c64f24db6c1cb0c26218bc5ba
Alaa Anani <alaa.a.anani@aucegypt.edu>
2021-05-21 16:16:22 +0200

    update

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ from PIL import ImageOps
 
 from keras.utils.vis_utils import plot_model
 
-from tensorflow import keras
+from tensorflow import keras 
 import numpy as np
 from tensorflow.keras.preprocessing.image import load_img
 from utils import read_dataset
@@ -20,98 +20,12 @@ import tensorflow as tf
 
 from skimage.io import imsave
 
-
+from model_utils import get_model, f1_m, recall_m, precision_m
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"      # To disable using GPU
 tf.get_logger().setLevel('ERROR')
 warnings.filterwarnings('ignore')
 
-#%%
 
-def get_model(img_size, num_classes):
-    inputs = keras.Input(shape=img_size + (1,))
-
-    ### [First half of the network: downsampling inputs] ###
-
-    # Entry block
-    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    previous_block_activation = x  # Set aside residual
-
-    # Blocks 1, 2, 3 are identical apart from the feature depth.
-    for filters in [64, 128, 256]:
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-
-        # Project residual
-        residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])  # Add back residual
-        previous_block_activation = x  # Set aside next residual
-
-    ### [Second half of the network: upsampling inputs] ###
-
-    for filters in [256, 128, 64, 32]:
-        x = layers.Activation("relu")(x)
-        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.Activation("relu")(x)
-        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.UpSampling2D(2)(x)
-
-        # Project residual
-        residual = layers.UpSampling2D(2)(previous_block_activation)
-        residual = layers.Conv2D(filters, 1, padding="same")(residual)
-        x = layers.add([x, residual])  # Add back residual
-        previous_block_activation = x  # Set aside next residual
-
-    # Add a per-pixel classification layer
-    outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(x)
-
-    # Define the model
-    model = keras.Model(inputs, outputs)
-    return model
-
-from keras import backend as K
-
-def recall_m(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(tf.math.multiply(y_true, y_pred), 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    recall = true_positives / (possible_positives + K.epsilon())
-    return recall
-
-def precision_m(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(tf.math.multiply(y_true, y_pred), 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    return precision
-
-def f1_m(y_true, y_pred):
-    
-    mask = tf.math.argmax(y_pred, axis=-1)
-    y_pred = tf.expand_dims(mask, axis=-1)
-    y_pred = tf.cast(y_pred, float)
-
-    y_pred = tf.reshape(y_pred, shape=(tf.shape(y_pred)[0], 256,256))
-
-    y_pred = tf.squeeze(y_pred)
-    y_true = tf.squeeze(y_true)
-
-    precision = precision_m(y_true, y_pred)
-    recall = recall_m(y_true, y_pred)
-    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 from utils import read_dataset
 
@@ -127,7 +41,7 @@ model = get_model(img_size, num_classes)
 
 #plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
 
-model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=[f1_m])
+model.compile(optimizer="sgd", loss=tf.losses.SigmoidFocalCrossEntropy(), metrics=[f1_m])
 
 callbacks = [
     keras.callbacks.ModelCheckpoint("unet_skel.h5", save_best_only=True)
