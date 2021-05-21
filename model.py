da4f8ec89bd4ed03cd21b5850036b081a9cbbfea
Oschart <oscar@aucegypt.edu>
2021-05-21 02:40:23 +0200

    Modified metrics to F1

--- model.py
+++ model.py
@@ -21,6 +21,8 @@ import warnings
 
 import tensorflow as tf
 
+from skimage.io import imsave
+
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"      # To disable using GPU
 tf.get_logger().setLevel('ERROR')
@@ -85,6 +87,35 @@ def get_model(img_size, num_classes):
     model = keras.Model(inputs, outputs)
     return model
 
+from keras import backend as K
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(tf.math.multiply(y_true, y_pred), 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(tf.math.multiply(y_true, y_pred), 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    
+    mask = tf.math.argmax(y_pred, axis=-1)
+    y_pred = tf.expand_dims(mask, axis=-1)
+    y_pred = tf.cast(y_pred, float)
+
+    y_pred = tf.reshape(y_pred, shape=(tf.shape(y_pred)[0], 256,256))
+
+    y_pred = tf.squeeze(y_pred)
+    y_true = tf.squeeze(y_true)
+
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
 from utils import read_dataset
 
 # Free up RAM in case the model definition cells were run multiple times
@@ -99,19 +130,37 @@ model = get_model(img_size, num_classes)
 
 #plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
 
-model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy")
+model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=[f1_m])
 
 callbacks = [
-    keras.callbacks.ModelCheckpoint("oxford_segmentation.h5", save_best_only=True)
+    keras.callbacks.ModelCheckpoint("unet_skel.h5", save_best_only=True)
 ]
 
 # Train the model, doing validation at the end of each epoch.
-epochs = 15
+epochs = 100
 
 x_train, x_test, y_train, y_test = read_dataset()
 
 
 model.fit(x_train, y_train, epochs=epochs, validation_data=(x_test,y_test), callbacks=callbacks, batch_size=32)
+
+
+
+new_model = keras.models.load_model('unet_skel.h5')
+
+Y = new_model.predict(x_test)
+
+#%%
+print(Y[0].shape)
+
+#%%%
+for i, y in enumerate(Y):
+    print()
+    mask = np.argmax(y, axis=-1)
+    mask = np.expand_dims(mask, axis=-1)
+    imsave(f'Y_val/{i}.png', mask)
+
+
 
 #model.summary()
 
