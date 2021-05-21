0f8d8b72d2780304513b9c688805a3bc58ca4233
M.Ghanem <mohamed.ghanem@nabtahealth.com>
2021-05-21 17:26:26 +0200

    Added U-shaped net

--- model.py
+++ model.py
@@ -87,6 +87,64 @@ def get_model(img_size, num_classes):
     model = keras.Model(inputs, outputs)
     return model
 
+def get_model_unet(img_size, num_classes):
+    inputs = keras.Input(shape=img_size + (1,))
+
+    prev_layers = {}
+    ### [First half of the network: downsampling inputs] ###
+
+    # Entry block
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    prev_layers[32] = x
+
+    # Blocks 1, 2, 3 are identical apart from the feature depth.
+    for filters in [64, 128, 256]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        prev_layers[filters] = x
+
+
+    ### [Second half of the network: upsampling inputs] ###
+
+    for filters in [256, 128, 64, 32]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.UpSampling2D(2)(x)
+
+        # Project residual
+        residual = layers.UpSampling2D(2)(prev_layers[filters])
+        # residual = layers.Conv2D(filters, 1, padding="same")(residual)
+        x = layers.concatenate([residual, x])  # Add back residual
+
+    # Add a per-pixel classification layer
+    outputs = layers.Conv2D(num_classes, 1, activation="softmax", padding="same")(x)
+
+    # Define the model
+    model = keras.Model(inputs, outputs)
+    return model
+
+
 from keras import backend as K
 
 def recall_m(y_true, y_pred):
@@ -126,9 +184,9 @@ img_size = (256, 256)
 num_classes = 2
 batch_size = 32
 # Build model
-model = get_model(img_size, num_classes)
+model = get_model_unet(img_size, num_classes)
 
-#plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+# plot_model(model, to_file='model_plot_unet.png', show_shapes=True, show_layer_names=True)
 
 model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=[f1_m])
 
@@ -142,7 +200,7 @@ epochs = 100
 x_train, x_test, y_train, y_test = read_dataset()
 
 
-model.fit(x_train, y_train, epochs=epochs, validation_data=(x_test,y_test), callbacks=callbacks, batch_size=32)
+# model.fit(x_train, y_train, epochs=epochs, validation_data=(x_test,y_test), callbacks=callbacks, batch_size=32)
 
 
 
